c71a00aa75b37c51b0b59bc7d535c4b1cbdc5a93
Anurag <kr.anurag24@gmail.com>
2021-09-05 19:46:09 +0530

    desc

--- pages/index.jsx
+++ pages/index.jsx
@@ -41,6 +41,10 @@ export default function Home() {
           type="image/x-icon"
         />
         <title>Dictionary</title>
+        <meta
+          name="description"
+          content="A Web-App to get meaning, example and pronounciation of any word!!"
+        />
         <link
           href="https://fonts.googleapis.com/css?family=Poppins:100,100italic,200,200italic,300,300italic,regular,italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic"
           rel="stylesheet"
